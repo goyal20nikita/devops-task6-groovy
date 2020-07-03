ad3820e8cc327111a8fb047cb9306c42b09ebc28
goyal20nikita <51130898+goyal20nikita@users.noreply.github.com>
2020-07-03 14:46:21 +0530

    Delete devops_task6_job1.sh

--- devops_task6_job1.sh
+++
@@ -1,7 +0,0 @@
-                        if ls /root/ | grep task6
-                        then
-                        cp -rvf * /root/task6
-                        else
-                        mkdir /root/task6
-                        cp -rvf */root/task6
-                        fi
